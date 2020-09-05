70c84d94396edeef61d6333151f02f0f57286f40
moh16247 <moh16247@iiitd.ac.in>
2020-09-05 15:31:33 +0530

    app folder created.

---
+++ src/app/app.module.ts
@@ -0,0 +1,72 @@
+import { BrowserModule, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
+
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import 'hammerjs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+
+import {DishService} from './services/dish.service';
+import {PromotionService} from './services/promotion.service';
+import {LeaderService} from './services/leader.service';
+
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
+//import { GestureConfig } from '@angular/material';
+import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ReactiveFormsModule } from '@angular/forms';
+//import { MatSlideToggle } from '@angular/material/slide-toggle';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    DishdetailComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent,
+    ContactComponent,
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatListModule,
+    FlexLayoutModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSelectModule,
+    MatSlideToggleModule,
+    MatFormFieldModule,
+    MatInputModule,
+    
+  ],
+  exports :[ MatSlideToggleModule, MatSliderModule, MatSelectModule ],
+  providers: [DishService, PromotionService,LeaderService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
